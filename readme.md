a128fae5f9f1b006b36454ec3e8515aa8434ea8d
unknown <1510386407@qq.com>
2020-03-05 14:51:28 +0800

    v1.01 automatic image segmentation

--- readme.md
+++ readme.md
@@ -3,3 +3,79 @@
 ## vue中引入assets里面图片的问题？
 
 当路径的文件名需要拼接变量的时候，可使用require()引入。
+
+## git命令
+
+### tag
+
+// 控制台打出当前仓库的所有标签
+git tag
+
+// 打标签，tag 自定义标识 版本id -m "备注"
+git tag v1.00 440376 -m "2020第一版"
+
+| 查看版本id git rev-parse HEAD ，查看段版本id git rev-parse --short HEAD
+
+// 将v1.00标签提交到git服务器
+git push origin v1.00
+
+// 将本地所有标签一次性提交到git服务器
+git push origin --tags
+
+// 切换标签
+git checkout tagname
+
+// 当前目录全部代码切换到v1.00时的代码
+// git checkout v1.00 
+
+// 删除标签
+git tag -d v1.00
+
+// 删除git服务器上的标签
+git push origin :v1.00
+
+## 切割图片相关
+
+canvas 默认300 X 150
+
+canvas的width和height跟style中定义的width和height是不同的，canvas标签的width和height是画布实际宽度和高度，
+绘制的图形都是在这个上面。style的width和height是canvas在浏览器中被渲染的高度和宽度。
+
+使用CSS设置画布的大小会导致画布按比例缩放你设置的值（CSS设置的宽高和300 * 150 的比值）
+
+例如，用CSS设置canvas 的宽高为450*300，对应默认的画布大小300 * 150 ，宽高比分别为 1.5和2 ，所以画布中100*100的图片会显示为宽100*1.5 150px， 高100*2 200px。
+
+改变canvas 的默认大小
+
+```html
+// 方法一
+<canvas width="100px" height="100px"></canvas>
+
+// 方法二，通过js的方法实现
+canvas.width="100"
+canvas.height="100"
+```
+
+## JS更新DOM后，页面没有及时渲染的问题？（JS线程阻塞和解决办法）
+
+两个概念：
+
+1. js是同步加载的
+2. 浏览器执行js程序，高于页面的渲染
+
+于是就出现了js更新DOM，页面不能及时渲染的问题。
+
+例如：
+
+```html
+document.body.html('helloworld')
+
+// 程序
+$.ajax({
+  ...
+})
+
+// 运行结果会在ajax执行完毕后，再去渲染页面
+```
+
+
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,13 +3,16 @@
     <ul class="puzzle-wrap">
       <li v-for="(puzzle, index) in puzzles" 
       :key="index"
-      
-      :class="{'puzzle': true, 'puzzle-empty': !puzzle.id}"
-      :style="{ background: 'url(' + puzzle.path + ')' }"
-      @click="moveFn(index)"
+      :class="{'puzzle': true, 'puzzle-empty': !puzzle}"
+      @click="moveFn(index, puzzle)"
       >
+      <canvas width="100px" height="100px" :id="'canvas' + puzzle"></canvas>
       </li>
     </ul>
+    <button
+    class="reset"
+    @click="reset"
+    >重新开始</button>
   </div>
 </template>
 
@@ -18,31 +21,57 @@
 export default {
   data(){
     return { 
-      puzzles: []
+      puzzles: [],
+      imgURL: require('../assets/dy_full.png')
     }
   },
   methods: {
+    reset(){
+      this.reRender();
+      this.showImage();
+    },
     reRender() {
       let puzzleArr = [];
 
       for(let i = 1; i < 9; i++) {
-        puzzleArr.push({id: i, path: require("../assets/dy" + i + ".png")});
+        puzzleArr.push(i);
       }
 
       // 随机打乱数组
-
       puzzleArr = puzzleArr.sort(() => {
         return Math.random() - 0.5;
       })
 
       // 页面显示
       this.puzzles = puzzleArr;
-      this.puzzles.push({id: '', path: ''});
-      console.log(this.puzzles)
+      this.puzzles.push('');
+    },
+
+    // 图片显示
+    showImage() {
+      // 图像切割
+      let image = new Image();
+      console.log(this.imgURL)
+      image.src = this.imgURL;
+      let k = 1;
+      for(let i = 0; i < 3; i++) {
+        for(let j = 0; j < 3; j++){
+          this.$nextTick(function(){
+            let canvas = document.querySelector("#canvas" + k++)
+            if(canvas) {
+              let ctx = canvas.getContext("2d");
+              // 将js线程挂起。让gui线程执行。
+              setTimeout(() => {
+                ctx.drawImage(image, j * 100, i * 100, 100, 100, 0, 0, 100, 100)
+              }, 1)
+            }
+          })
+        }
+      }
     },
 
     // 移动方法
-    moveFn(index) {
+    moveFn(index, puzzle) {
       // 获取点击位置及其上下左右的值
       let curNum = this.puzzles[index],
           topNum = this.puzzles[index - 3],
@@ -50,28 +79,41 @@ export default {
           leftNum = this.puzzles[index - 1],
           rightNum = this.puzzles[index + 1];
       // 如果左侧为空，和左侧交换位置
-      if(leftNum && leftNum.id === '') {
+      
+      let ischange = false;
+      if(leftNum === '') {
         this.$set(this.puzzles, index - 1, curNum)
-        this.$set(this.puzzles, index, {id: '', path: ''})
-      } else if(rightNum && rightNum.id === '') {
+        this.$set(this.puzzles, index, '')
+        ischange = true;
+      } else if(rightNum === '') {
         this.$set(this.puzzles, index + 1, curNum)
-        this.$set(this.puzzles, index, {id: '', path: ''})
-      } else if(topNum && topNum.id === '') {
+        this.$set(this.puzzles, index, '')
+        ischange = true;
+      } else if(topNum === '') {
         this.$set(this.puzzles, index -3, curNum)
-        this.$set(this.puzzles, index, {id: '', path: ''})
-      } else if(bottomNum && bottomNum.id === '') {
+        this.$set(this.puzzles, index, '')
+        ischange = true;
+      } else if(bottomNum === '') {
         this.$set(this.puzzles, index + 3, curNum)
-        this.$set(this.puzzles, index, {id: '', path: ''})
+        this.$set(this.puzzles, index, '')
+        ischange = true;
       }
-      console.log(this.puzzles)
-      this.passFn();
+      if(ischange) {
+        let clickImg = document.querySelector('#canvas' + puzzle);
+        let blankImg = document.querySelector('#canvas')
+        let ctx0 = clickImg.getContext('2d');
+        let ctx1 = blankImg.getContext('2d');
+        ctx1.drawImage(clickImg, 0, 0);
+        ctx0.clearRect(0, 0, 100, 100)
+        this.passFn();
+      }
     },
 
     // 判断成功
     passFn() {
-      if(this.puzzles[this.puzzles.length - 1].id === '') {
+      if(this.puzzles[this.puzzles.length - 1] === '') {
         let puzzleArr = this.puzzles.slice(0, this.puzzles.length - 1);
-        const isOK = puzzleArr.every((val, index) => val.id === index + 1);
+        const isOK = puzzleArr.every((val, index) => val === index + 1);
         if(isOK) {
           setTimeout(()=> alert('*-*党艳真棒*-*'), 20)  
         }
@@ -80,6 +122,9 @@ export default {
   },
   created() {
     this.reRender();
+  },
+  mounted() {
+    this.showImage();
   }
 }
 </script>
@@ -104,7 +149,6 @@ export default {
   float: left;
   font-size: 40px;
   color: white;
-  background-color: #f90;
   text-align: center;
   line-height: 100px;
   border: 1px solid #ccc;
@@ -116,5 +160,15 @@ export default {
 .puzzle-empty {
   background: #ccc;
   box-shadow: inset 2px 2px 18px;
+}
+
+.reset {
+  background-color: #f90;
+  color: #ffffff;
+  width: 300px;
+  font-size: 20px;
+  height: 35px;
+  line-height: 35px;
+  border-radius: 5px;
 }
 </style>
